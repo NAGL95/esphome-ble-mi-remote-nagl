ced21ca817390676a3092a4b2e3cb7a34e610f69
NAGL95 <134092353+NAGL95@users.noreply.github.com>
2026-08-23 18:38:18 +0000

    fix12

--- components/ble_mi_remote/ble_mi_remote.h
+++ components/ble_mi_remote/ble_mi_remote.h
@@ -60,6 +60,7 @@ namespace esphome {
 
 				void setup() override;
 				void update() override;
+				void applyAdvertisementData();
 
 				float get_setup_priority() const override { return setup_priority::AFTER_BLUETOOTH; }
 
