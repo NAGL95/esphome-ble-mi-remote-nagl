62729871eae1cc93a2e247abe297d13c7bfa51ab
Nikita <n.a.gl@yandex.ru>
2026-09-20 21:38:09 +0300

    fix7

--- components/ble_mi_remote/ble_mi_remote.cpp
+++ components/ble_mi_remote/ble_mi_remote.cpp
@@ -280,8 +280,9 @@ namespace esphome {
       if (this->_reconnect) {
         pServer->advertiseOnDisconnect(true);
       }
-
-      pServer->startAdvertising();
+      applyAdvertisementData(false);
+      advertising->stop();
+      advertising->start();
     }
 
     void BleMiRemote::update() { state_sensor_->publish_state(this->_connected); }
@@ -573,6 +574,12 @@ namespace esphome {
     void BleMiRemote::onDisconnect(NimBLEServer *pServer, NimBLEConnInfo& connInfo, int reason) {
       this->_connected = false;
       if (this->_reconnect) {
+        applyAdvertisementData(false);  this->_connected = false;
+  if (this->_reconnect) {
+    applyAdvertisementData(false);
+    pServer->startAdvertising();
+  }
+}
         pServer->startAdvertising();
       }
     }
